370bc3dcaa4410a19856f3a742e6d5f3b575f0a3
D.Matt Placek <atomicsuntan@gmail.com>
2016-07-31 19:19:30 +0000

    explicitly disable tryjit, renable win test on thredsafe locking

--- bang.h
+++ bang.h
@@ -19,7 +19,7 @@
 #define LCFG_UPVAL_SIMPLEALLOC 1
 
 #ifndef LCFG_MT_SAFEISH
-# if defined __GNUC__ // (WIN32) && __GNUC__
+# if defined(WIN32) && __GNUC__
 #  define LCFG_MT_SAFEISH 0 // no threading/nylon for mingw build, so don't bother!
 # else
 #  define LCFG_MT_SAFEISH 1
